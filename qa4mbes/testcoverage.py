bd57ea8f38a75b8911e712d036064366aa10627e
Adam <adam.d.steer@gmail.com>
2019-02-12 11:25:08 +1100

    moving goalposts a little - starting to design per-file-format coverage extractors

--- qa4mbes/testcoverage.py
+++ qa4mbes/testcoverage.py
@@ -3,6 +3,7 @@
 #standard library
 import os
 import json
+import re
 
 #do we need to parse arguments...
 # yes! if we're calling from the CLI
@@ -15,17 +16,42 @@ from shapely import geometry
 import getcoverage
 
 
+parser = ArgumentParser()
+parser.add_argument("-i", "--input-file",
+                    help="input filename for coverage extraction")
+
+parser.add_argument("-r", "--referencepolygon",
+                    help="refernce polygon filename")
+
+# input CRS for each should also nbe options... coming!
+
+# unpack arguments
+args = parser.parse_args()
+
+inputfile = vars(args)["input_file"]
+referencepolygon = vars(args)["referencpolygon"]
+
+
 def testcoverage(planningpolygon, surveyswath):
     """
-    Given an OGR-compatible polygon and a survey swath, return:
+    Given an OGR-compatible polygon and a survey swath file name, return:
     - % of reference polygon covered by survey
     - GeoJSON polygon describing intersection of reference and survey
     - distance between reference and test coverage centroids, in metres
     """
 
     #this may take a while
-    surveycoverage = getcoverage(surveyswath)
+    if (re.search("*\.xyz$", surveyswath)):
+        surveycoverage = getcoverage.xyzcoverage(surveyswath)
 
+    elif (re.search("*\.las|\.laz$", surveyswath)):
+        surveycoverage = getcoverage.lascoverage(surveyswath)
+
+    elif (re.search("*\.tif|\.TIF|\.tiff$", surveyswath)):
+        surveycoverage = getcoverage.tifcoverage(surveyswath)
+
+    else (re.search("*\.bag|.BAG$", surveyswath)):
+        surveycoverage = getcoverage.bagcoverage(surveyswath)
 
     #return a dictionary, ready to write out as JSON
     return testdata
